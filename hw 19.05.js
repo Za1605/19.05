eba8d76d35f323c45f2106cf7e80c5af80e06c1c
YuliiaT <115080417+Za1605@users.noreply.github.com>
2023-07-18 00:06:02 +0300

    first commit

---
+++ hw 19.05.js
@@ -0,0 +1,54 @@
+// Знайти та вивести довижину настипних стрінгових значень
+//'hello world', 'lorem ipsum', 'javascript is cool'
+
+let hel = 'hello world';
+ console.log(hel.length);
+
+ let lor = 'lorem ipsum';
+ console.log(lor.length);
+
+ const java = 'lavascript is cool';
+ console.log(java.length);
+
+
+// Перевести до великого регістру наступні стрінгові значення
+//'hello world', 'lorem ipsum', 'javascript is cool'
+
+let hel1 = 'hello world';
+
+let upperCase = hel1.toUpperCase();
+console.log(upperCase);
+
+let hel2 = 'lorem ipsum';
+
+let upperCase1 = hel2.toUpperCase();
+console.log(upperCase1);
+
+let hel3 = 'javascript is cool';
+
+let upperCase2 = hel3.toUpperCase();
+console.log(upperCase2);
+
+// Перевести до нижнього регістру настипні стрінгові значення
+//HELLO WORLD', 'LOREM IPSUM', 'JAVASCRIPT IS COOL'
+
+let HEL = 'HELLO WORLD'
+let lowerCase = HEL.toLowerCase();
+console.log(lowerCase);
+
+let HEL1 = 'LOREM IPSUM'
+let lowerCase1 = HEL1.toLowerCase();
+console.log(lowerCase1);
+
+let HEL2 = 'JAVASCRIPT IS COOL'
+let lowerCase2 = HEL2.toLowerCase();
+console.log(lowerCase2);
+
+
+// Є "брудна" стрінга let str = ' dirty string   ' . Почистити її від зайвих пробілів.
+
+let str = ' dirty string';
+
+let substring = str.substring(1,13);
+ console.log(substring);
+
